da4fe3909b3848a83276d2b1b56863a6a7dc3c27
Ruairidh Holwill <roore@hotmail.co.uk>
2019-06-07 16:39:52 +0100

    fixed spelling

--- client/src/views/letter_entry_view.js
+++ client/src/views/letter_entry_view.js
@@ -20,7 +20,7 @@ LetterEntryView.prototype.bindEvents = function(){
 LetterEntryView.prototype.render = function (){
     const form = document.createElement('form');
     const label = document.createElement('label')
-    label.textContent = "Entry your letter choice:"
+    label.textContent = "Enter your letter choice:"
     form.appendChild(label);
     const letterEntry = document.createElement('input');
     letterEntry.type = 'text'
