1ea6e35e8f676ae49e35e71f2934c4c5e59d09cd
Ruairidh Holwill <roore@hotmail.co.uk>
2019-06-07 19:14:52 +0100

    button appears on win/lose screen

--- client/src/views/result_view.js
+++ client/src/views/result_view.js
@@ -7,6 +7,11 @@ ResultView.prototype.loseGameRender = function(){
     const loseMessage = document.createElement('h1')
     loseMessage.textContent = 'YOU LOSE'
     this.container.appendChild(loseMessage)
+    let playAgainButton = document.createElement('input')
+    playAgainButton.type = "submit"
+    playAgainButton.value = "Play Again!"
+    this.container.appendChild(playAgainButton)
+
 }
 
 ResultView.prototype.winGameRender = function(){
@@ -14,6 +19,10 @@ ResultView.prototype.winGameRender = function(){
     const winMessage = document.createElement('h1')
     winMessage.textContent = 'YOU WIN'
     this.container.appendChild(winMessage)
+    let playAgainButton = document.createElement('input')
+    playAgainButton.type = "submit"
+    playAgainButton.value = "Play Again!"
+    this.container.appendChild(playAgainButton)
 }
 
 
